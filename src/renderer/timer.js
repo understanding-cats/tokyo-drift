16f89cb38f831bfeae16936db093159dd4b3fe91
emugnier <emugnier@ucsd.edu>
2020-11-23 17:13:02 +0100

    add doc and struct of JS code

---
+++ src/renderer/timer.js
@@ -0,0 +1,33 @@
+/**
+ * Starts the timer and create the session accordingly
+ * (UI changes are also handle there)
+ * @param {string} name - Name of the session
+ * @returns {boolean} - True if success else False
+ */
+function startTimer(name) {
+  //
+}
+
+/**
+ * Pause the timer
+ * @returns {boolean} - True if success else False
+ */
+function pauseTimer() {
+  //
+}
+
+/**
+ * Unpause the timer
+ * @returns {boolean} - True if success else False
+ */
+function unpauseTimer() {
+  //
+}
+
+/**
+ * Cancel the timer
+ * @returns {boolean} - True if success else False
+ */
+function cancelTimer() {
+  //
+}
---
+++ src/storage/Session.js
@@ -0,0 +1,38 @@
+/** Pomodoro session class.
+ * @class
+ * @param {string} name - Name of the session.
+ * @property {string} start_time - Starting time of the session
+ * @property {string} end_time - End time of the session
+ * @property {number} nb_work_periods - Number of work periods
+ * @property {number} break_duration - Break duration in minutes
+ * @property {number} work_period_duration - Work period duration in minutes
+ */
+class Session {
+  constructor(name) {
+    // ...
+  }
+
+  /**
+   * End a session.
+   * @return {boolean} True if success else False
+   */
+  end() {
+  // ...
+  }
+
+  /**
+   * Store the session in persistent data.
+   * @return {number} id of the session if success.
+   */
+  store() {
+  // ...
+  }
+
+  /**
+   * Get the session in persistent data.
+   * @return {number} id of the session if success.
+   */
+  getFromLocalStorage() {
+    // ..
+  }
+}
