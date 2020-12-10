8cfc12cb451e53b0a479888620d7c6b6c2ef4a41
Marcus Fedarko <mfedarko@umd.edu>
2020-12-09 19:51:51 -0800

    STY: switch to camelCase in a file

--- src/js/setsession.js
+++ src/js/setsession.js
@@ -5,26 +5,26 @@ function goToPomo() {
   location.href = 'html/pomodoro.html';
 }
 function getsettings() {
-  const work_insec = localStorage.getItem('work_ls') || 25 * 60;
-  const sbreak_insec = localStorage.getItem('sbreak_ls') || 5 * 60;
-  const lbreak_insec = localStorage.getItem('lbreak_ls') || 15 * 60;
-  const work_periods = localStorage.getItem('periods_ls') || 4;
-  document.getElementById('pomo_length').value = work_insec / 60;
-  document.getElementById('sbreak_length').value = sbreak_insec / 60;
-  document.getElementById('lbreak_length').value = lbreak_insec / 60;
-  document.getElementById('work_periods').value = work_periods;
+  const workInSec = localStorage.getItem('work_ls') || 25 * 60;
+  const sbreakInSec = localStorage.getItem('sbreak_ls') || 5 * 60;
+  const lbreakInSec = localStorage.getItem('lbreak_ls') || 15 * 60;
+  const workPeriods = localStorage.getItem('periods_ls') || 4;
+  document.getElementById('pomo_length').value = workInSec / 60;
+  document.getElementById('sbreak_length').value = sbreakInSec / 60;
+  document.getElementById('lbreak_length').value = lbreakInSec / 60;
+  document.getElementById('work_periods').value = workPeriods;
   displaySessionInfo();
 }
 
 function displaySessionInfo() {
-  const work_insec = localStorage.getItem('work_ls') || 25 * 60;
-  const sbreak_insec = localStorage.getItem('sbreak_ls') || 5 * 60;
-  const lbreak_insec = localStorage.getItem('lbreak_ls') || 15 * 60;
-  const work_periods = localStorage.getItem('periods_ls') || 4;
-  document.getElementById('worktime').innerText = `Work Session: ${work_insec / 60}mins`;
-  document.getElementById('shortbreak').innerText = `Short Break: ${sbreak_insec / 60}mins`;
-  document.getElementById('longbreak').innerText = `Long Break: ${lbreak_insec / 60}mins`;
-  document.getElementById('workp').innerText = `Work Periods: ${work_periods}`;
+  const workInSec = localStorage.getItem('work_ls') || 25 * 60;
+  const sbreakInSec = localStorage.getItem('sbreak_ls') || 5 * 60;
+  const lbreakInSec = localStorage.getItem('lbreak_ls') || 15 * 60;
+  const workPeriods = localStorage.getItem('periods_ls') || 4;
+  document.getElementById('worktime').innerText = `Work Session: ${workInSec / 60}mins`;
+  document.getElementById('shortbreak').innerText = `Short Break: ${sbreakInSec / 60}mins`;
+  document.getElementById('longbreak').innerText = `Long Break: ${lbreakInSec / 60}mins`;
+  document.getElementById('workp').innerText = `Work Periods: ${workPeriods}`;
 }
 
 /**
@@ -33,14 +33,14 @@ function displaySessionInfo() {
  */
 function startsess() {
   if (typeof Storage !== 'undefined') {
-    const work_insec = 60 * document.getElementById('pomo_length').value;
-    const sbreak_insec = 60 * document.getElementById('sbreak_length').value;
-    const lbreak_insec = 60 * document.getElementById('lbreak_length').value;
-    const work_periods = document.getElementById('work_periods').value;
-    localStorage.setItem('work_ls', work_insec);
-    localStorage.setItem('sbreak_ls', sbreak_insec);
-    localStorage.setItem('lbreak_ls', lbreak_insec);
-    localStorage.setItem('periods_ls', work_periods);
+    const workInSec = 60 * document.getElementById('pomo_length').value;
+    const sbreakInSec = 60 * document.getElementById('sbreak_length').value;
+    const lbreakInSec = 60 * document.getElementById('lbreak_length').value;
+    const workPeriods = document.getElementById('work_periods').value;
+    localStorage.setItem('work_ls', workInSec);
+    localStorage.setItem('sbreak_ls', sbreakInSec);
+    localStorage.setItem('lbreak_ls', lbreakInSec);
+    localStorage.setItem('periods_ls', workPeriods);
     document.getElementById('SetSessionModal').style.display = 'none';
     displaySessionInfo();
   } else {
