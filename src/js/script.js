e194b3ff20cdfba1a89341df87db106501d5b8f4
yf855 <56779633+yf855@users.noreply.github.com>
2020-12-09 16:24:50 -0800

    Update src/js/script.js
    Co-authored-by: Marcus Fedarko <mfedarko@ucsd.edu>

--- src/js/script.js
+++ src/js/script.js
@@ -197,15 +197,3 @@ function cancel_all() {
 function clear_all() {
   location.reload();
 }
-
-
-// function doConfirm(msg, yesFn, noFn) {
-//   var confirmBox = $("#confirmBox");
-//   confirmBox.find(".message").text(msg);
-//   confirmBox.find(".yes,.no").unbind().click(function () {
-//       confirmBox.hide();
-//   });
-//   confirmBox.find(".yes").click(yesFn);
-//   confirmBox.find(".no").click(noFn);
-//   confirmBox.show();
-// }
