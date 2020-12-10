25f844f235b3a7cf854792571f5c7b0fa6a6b9a8
Marcus Fedarko <mfedarko@umd.edu>
2020-12-09 19:59:49 -0800

    STY: switch to camelCase in savesession.js

--- src/js/savesession.js
+++ src/js/savesession.js
@@ -4,29 +4,29 @@ function loadHistory(path) {
   // document.getElementById("dummyBtn").innerHTML = jsonData.records[0].task;
 
   for (const r of allrecords) {
-    const start_date = new Date(r.date);
+    const startDate = new Date(r.date);
 
     // var itmbtn = document.getElementById("dummyBtn");
     const clnbtn = document.getElementById('dummyBtn').cloneNode(true);
     clnbtn.removeAttribute('ID');
-    clnbtn.innerText = start_date.toDateString();
+    clnbtn.innerText = startDate.toDateString();
     // var itmctn = document.getElementById("dummyContent");
     const clnctn = document.getElementById('dummyContent').cloneNode(true);
     clnctn.removeAttribute('ID');
     clnctn.innerText = '';
 
-    const task_name = document.createElement('P');
-    task_name.innerText = `Task: ${r.task}`;
-    const start_time = document.createElement('P');
-    start_time.innerText = `Started work at ${r.start}`;
-    const work_len = document.createElement('P');
-    work_len.innerText = `${r.periods} work periods of length ${r.wlength / 60} minutes`;
-    const break_len = document.createElement('P');
-    break_len.innerText = `separated by ${r.blength / 60}-minute breaks.`;
-    clnctn.appendChild(task_name);
-    clnctn.appendChild(start_time);
-    clnctn.appendChild(work_len);
-    clnctn.appendChild(break_len);
+    const taskName = document.createElement('P');
+    taskName.innerText = `Task: ${r.task}`;
+    const startTime = document.createElement('P');
+    startTime.innerText = `Started work at ${r.start}`;
+    const workLen = document.createElement('P');
+    workLen.innerText = `${r.periods} work periods of length ${r.wlength / 60} minutes`;
+    const breakLen = document.createElement('P');
+    breakLen.innerText = `separated by ${r.blength / 60}-minute breaks.`;
+    clnctn.appendChild(taskName);
+    clnctn.appendChild(startTime);
+    clnctn.appendChild(workLen);
+    clnctn.appendChild(breakLen);
 
     document.getElementById('HistoryModalContent').appendChild(clnbtn);
     document.getElementById('HistoryModalContent').appendChild(clnctn);
